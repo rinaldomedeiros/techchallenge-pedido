29e7d39a3e27a45afbeb120d6e57c010c9037137
Waltson Silva <waltson.dasilva@gmail.com>
2025-02-04 23:34:48 -0300

    implementation of generation dateOrder

---
+++ src/main/kotlin/br/com/fiap/techchallenge/orders/config/MongoConfig.kt
@@ -0,0 +1,9 @@
+package br.com.fiap.techchallenge.orders.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+
+@Configuration
+@EnableMongoAuditing
+class MongoConfig {
+}
